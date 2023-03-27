c0bde9118d040f68707f2838902cabbac67b8c62
kogure-yuka <123721697+kogure-yuka@users.noreply.github.com>
2023-03-27 22:40:55 +0900

    練習問題1

--- 3-2/練習問題1.php
+++ 3-2/練習問題1.php
@@ -1,30 +1,35 @@
 
 <?php
-  
-$fruits=["apple"=>"りんご","orange"=>"みかん","peach"=>"もも"];
 
 function getprice($quantity,$unitprice){
   $price =$quantity * $unitprice;
+
+  $fruits=["apple"=>"りんご","orange"=>"みかん","peach"=>"もも"];
+
+  foreach($fruits as $key=>$value){
 
   echo $value."は".$price."円です。";
   echo'<br>';
  
 }
-
+}
 
 getprice(2,150);
 getprice(3,50);
 getprice(6,500);
+echo'<br>';
 
-foreach($fruits as $key=>$value){
-  
- echo $value."は".$price."円です。";
- echo'<br>';
-}
 ?>
 
 
-
-
+<?php
+function fruits($fruit,$count,$price){
+   echo $fruit.'は'.$count*$price.'円です。<br>';
+  }
+  
+   fruits('りんご',2,150);
+   fruits('みかん',3,50);
+   fruits('もも',6,500);
+   ?>
 
 

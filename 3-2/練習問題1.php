fedf75f94b99b03f7636ec06c24d1e6d3914be89
kogure-yuka <123721697+kogure-yuka@users.noreply.github.com>
2023-03-29 21:24:57 +0900

    練習問題１

--- 3-2/練習問題1.php
+++ 3-2/練習問題1.php
@@ -1,11 +1,24 @@
 <?php
-function fruits($fruit,$count,$price){
-   echo $fruit.'は'.$count*$price.'円です。<br>';
-  }
-  
-   fruits('りんご',2,150);
-   fruits('みかん',3,50);
-   fruits('もも',6,500);
-   ?>
+//フルーツと単価の連想配列
+$fruits=["りんご"=>150,"みかん"=>50,"もも"=>500];
+//個数の配列
+$quantity=[2,3,6];
+
+//単価を計算する関数を定義
+function getprice($count,$price){
+   //返り値は合計値を返す
+   return $count*$price;
+}
+
+$sum=getprice(2,150);
+   echo 'りんごは'.$sum.'円です。<br>';
+
+$sum=getprice(3,50);
+   echo 'みかんは'.$sum.'円です。<br>';
+
+$sum=getprice(6,500);
+   echo 'ももは'.$sum.'円です。<br>';
+
+?>
 
 
---
+++ 3-2/練習問題1引数３つ.php
@@ -0,0 +1,11 @@
+<?php
+function fruits($fruit,$count,$price){
+   echo $fruit.'は'.$count*$price.'円です。<br>';
+  }
+  
+   fruits('りんご',2,150);
+   fruits('みかん',3,50);
+   fruits('もも',6,500);
+   ?>
+
+
